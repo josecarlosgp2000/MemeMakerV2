12300c88a1ffa09025586d7a64617222537b134c
josecarlosgp <jotace300@gmail.com>
2021-11-04 15:56:42 +0100

    Agregar archivos de proyecto.

---
+++ MemeMakerV2/Convertidor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MemeMakerV2
+{
+    class Convertidor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? 2 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
